f2ce6d78e6db6be2020440a9bb395b33aaa250a4
shejun.zhou <shejunzhou@shejunzhoudeMacBook-Pro.local>
2015-02-10 12:58:11 +0800

    完善注释

--- ScroTT/AppDelegate.h
+++ ScroTT/AppDelegate.h
@@ -6,6 +6,18 @@
 //  Copyright (c) 2015年 YiBan.iOS. All rights reserved.
 //
 
+
+/**
+ @file          AppDelegate.h
+ 
+ @author		shejun.zhou
+ @version		1.0
+ @date          2015-02-09
+ @copyright     shejun.zhou
+ 
+ @brief         循环滚动 Demo
+ */
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- ScroTT/ViewController.h
+++ ScroTT/ViewController.h
@@ -6,6 +6,18 @@
 //  Copyright (c) 2015年 YiBan.iOS. All rights reserved.
 //
 
+
+/**
+ @file          ViewController.h
+ 
+ @author		shejun.zhou
+ @version		1.0
+ @date          2015-02-09
+ @copyright     shejun.zhou
+ 
+ @brief         该文件暂时作废
+ */
+
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
